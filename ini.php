bc3f01c8334b2470318afb131453a7be1924c7f8
Slim Amamou <slim.amamou@gmail.com>
2009-01-09 10:10:54 +0100

    nouveau ini.php avec base par defaut /test/waraq.db

--- ini.php
+++ ini.php
@@ -1,6 +1,11 @@
 <?php
+require 'lib/localresource.php';
 
-$ini['root'] = dirname(__FILE__);
-$ini['db'] = "sqlite:". $ini['root'] ."/test/waraq.db";
+$url  = "http://". $_SERVER['SERVER_NAME'] ."/". $_SERVER['REQUEST_URI'];
+$file = $_SERVER['SCRIPT_FILENAME'];
+$here = new LocalResource($url, $file);
+$root = $here->base()->get(ROOT);
+
+$ini['db'] = "sqlite:". $root->file ."/test/waraq.db";
 $ini['db.user'] = "";
 $ini['db.password'] = "";
