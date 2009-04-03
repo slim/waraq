fe029d7a0f01b4b45028085e8b1422ad79003bba
Slim Amamou <slim.amamou@gmail.com>
2009-04-03 17:02:47 +0100

    row.js library has been refactored by introducing ElementList class
    example: ROW = new ElementList('tr')

--- js/row.js
+++ js/row.js
@@ -1,60 +1,50 @@
-ROW = {
-	root: document,
-	rows: [],
-	current_row: null,
-	init: function(table) {
-		var tables, rows;
+function ElementList(element, root) {
+	this.root = root || document.body;
+	this.rows = [];
+	this.current_row = null;
 
-		ROW.rows = [];
-		if (table) {
-			tables = [table];
-		} else {
-			tables = ROW.root.getElementsByTagName('table');	
-		}
-		for (var t=0; t < tables.length; t++) {
-			rows = tables[t].getElementsByTagName('tr');
-			for (var r=0; r < rows.length; r++) {
-				if (rows[r].id) {
-					rows[r].ROW_index = ROW.rows.length;
-					rows[r].setAsCurrent = function() {
-						ROW.current_row = this.ROW_index;
-					}
-					ROW.rows[rows[r].ROW_index] = rows[r];
-				}
+	var document_tables, document_rows;
+
+	document_rows = this.root.getElementsByTagName(element);
+	for (var r=0; r < document_rows.length; r++) {
+		if (document_rows[r].id) {
+			document_rows[r].ROW_index = this.rows.length;
+			document_rows[r].TRlist = this;
+			document_rows[r].setAsCurrent = function() {
+				this.TRlist.current_row = this.ROW_index;
 			}
+			this.rows[document_rows[r].ROW_index] = document_rows[r];
 		}
-	},
-	first: function() {
-		if (ROW.rows.length < 1) {
-			ROW.init();
-		}
-		ROW.current_row = 0;
-
-		return ROW.rows[ROW.current_row];
-	},
-	current: function() {
-		return ROW.rows[ROW.current_row] || ROW.first();
-	},
-	next: function() {
-		if (ROW.current_row < ROW.rows.length - 1) {
-			ROW.current_row++;
-		}
-
-		if (ROW.onNext) {
-			ROW.onNext();
-		}
-
-		return ROW.rows[ROW.current_row];
-	},
-	previous: function() {
-		if (ROW.current_row > 0 ) {
-			ROW.current_row--;
-		}
-
-		if (ROW.onPrevious) {
-			ROW.onPrevious();
-		}
-
-		return ROW.rows[ROW.current_row];
 	}
+
+	this.first = function() {
+		this.current_row = 0;
+
+		return this.rows[this.current_row];
+	};
+	this.current = function() {
+		return this.rows[this.current_row] || this.first();
+	};
+	this.next = function() {
+		if (this.current_row < this.rows.length - 1) {
+			this.current_row++;
+		}
+
+		if (this.onNext) {
+			this.onNext();
+		}
+
+		return this.rows[this.current_row];
+	};
+	this.previous = function() {
+		if (this.current_row > 0 ) {
+			this.current_row--;
+		}
+
+		if (this.onPrevious) {
+			this.onPrevious();
+		}
+
+		return this.rows[this.current_row];
+	};
 }
