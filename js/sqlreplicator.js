a84e00b97aa8ae3cde620ebbc98bf4d123883c90
Slim Amamou <slim.amamou@gmail.com>
2009-04-08 09:19:15 +0100

    bugfix: sqlReplicator was executing anything he pulls without limit

--- js/sqlreplicator.js
+++ js/sqlreplicator.js
@@ -3,6 +3,7 @@ sqlReplicator = {
 	master_url: null,
 	master_pos: 0,
 	chunk_size: 100,
+	onComplete: null,
 
 	pull: function () {
 		new Ajax.Request(this.master_url, {
@@ -10,13 +11,18 @@ sqlReplicator = {
 			method: 'get',
 			onSuccess: function (transport) {
 				var queries = transport.responseText.split(';');
-				for(i=0; i < queries.length; i++) {
+				for(i=0; i < queries.length && i < sqlReplicator.chunk_size; i++) {
 					if (queries[i]) {
 						sqlReplicator.slave_db.execute(queries[i]);
 					}
 				}
 				if (queries.length < sqlReplicator.chunk_size) {
-					return;
+					if (sqlReplicator.onComplete) {
+						sqlReplicator.onComplete();
+					}
+					else {
+						return;
+					}
 				}
 				else {
 					sqlReplicator.master_pos += sqlReplicator.chunk_size;
