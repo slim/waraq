df7c10086d5292c8a3b94e45dec5001c364ea902
Slim Amamou <slim.amamou@gmail.com>
2009-07-16 16:54:30 +0100

    json2dom

---
+++ js/json2dom.js
@@ -0,0 +1,37 @@
+JSON2DOM = {
+	deserialize: function (json) {
+		eval("json = "+json);
+		return JSON2DOM.build(json);
+	},
+	type: function (obj) {
+		if (obj instanceof Array) {
+			return "array";
+		}
+		return typeof(obj);
+	},
+	build: function (child, parent) {
+		switch (JSON2DOM.type(child)) {
+		case 'string':
+			parent.appendChild(document.createTextNode(child));
+			break;
+		case 'object':
+			for (attribute in child) {
+				parent.setAttribute(attribute, child[attribute]);
+			}
+			break;
+		case 'array':
+			element = document.createElement(child[0]);
+			for (i=1; i < child.length ; i++) {
+				element = JSON2DOM.build(child[i], element);
+			}
+			if (parent) {
+				parent.appendChild(element);
+			}
+			else {
+				parent = element;
+			}
+			break;
+		}
+		return parent;
+	}
+}
