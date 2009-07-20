ad7db163f7b98b140b2bfdb3ffecd93acb760f1e
Slim Amamou <slim.amamou@gmail.com>
2009-07-20 15:07:05 +0100

    refactoting: now main function buildom()

--- js/json2dom.js
+++ js/json2dom.js
@@ -1,39 +1,40 @@
-JSON2DOM = {
-	deserialize: function (json) {
-		eval("json = "+json);
-		return JSON2DOM.build(json);
-	},
-	type: function (obj) {
+function buildom(child, parent) {
+	var element;
+
+	function unitype(obj) {
 		if (obj instanceof Array) {
 			return "array";
 		}
 		return typeof(obj);
-	},
-	build: function (child, parent) {
-		var element;
+	};
 
-		switch (JSON2DOM.type(child)) {
-		case 'string':
-			parent.appendChild(document.createTextNode(child));
-			break;
-		case 'object':
-			for (attribute in child) {
-				parent.setAttribute(attribute, child[attribute]);
-			}
-			break;
-		case 'array':
-			element = document.createElement(child[0]);
-			for (var i=1; i < child.length ; i++) {
-				JSON2DOM.build(child[i], element);
-			}
-			if (parent) {
-				parent.appendChild(element);
-			}
-			else {
-				parent = element;
-			}
-			break;
+	switch (unitype(child)) {
+	case 'string':
+		parent.appendChild(document.createTextNode(child));
+		break;
+	case 'object':
+		for (attribute in child) {
+			parent.setAttribute(attribute, child[attribute]);
 		}
-		return parent;
+		break;
+	case 'array':
+		element = document.createElement(child[0]);
+		for (var i=1; i < child.length ; i++) {
+			buildom(child[i], element);
+		}
+		if (parent) {
+			parent.appendChild(element);
+		}
+		else {
+			parent = element;
+		}
+		break;
+	}
+	return parent;
+};
+JSON2DOM = {
+	deserialize: function (json) {
+		eval("json = "+json);
+		return buildom(json);
 	}
 }
