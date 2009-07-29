9cc4cd639ac820e062b52b754f226f247b306ccf
Slim Amamou <slim.amamou@gmail.com>
2009-07-29 15:53:20 +0100

    clifform.js first -- make a form behave like a CLI

---
+++ js/clifform.js
@@ -0,0 +1,29 @@
+CLIFFORM = {
+	form: null,
+	elements: null,
+	keyBinding: function (e) {
+		var key = e.keyCode;
+		switch (key) {
+			case Event.KEY_RETURN:
+				$(CLIFFORM.elements.current()).hide();
+				if (CLIFFORM.elements.next()) {
+					$(CLIFFORM.elements.current()).show();
+					$(CLIFFORM.elements.current()).focus();
+				}
+				else {
+					CLIFFORM.form.submit();
+				}
+			break;
+		}
+	},
+	swallow: function (form) {
+		CLIFFORM.form = form;
+		CLIFFORM.elements = new ElementList;
+		CLIFFORM.elements.select(['label'], form);
+		while (CLIFFORM.elements.next() != null) {
+			$(CLIFFORM.elements.current()).hide();
+		}
+		Event.observe(window, 'keypress', CLIFFORM.keyBinding);
+		CLIFFORM.elements.first().focus();
+	}
+}
